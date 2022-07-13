d718bac9f6cceefff0df594eaad47fe58d8c924b
unknown <30100049@syntec.corp>
2022-07-13 09:06:20 +0800

    feat(vuex): change data source from mockdata to real database.

--- src/store/product/actions.ts
+++ src/store/product/actions.ts
@@ -1,20 +1,35 @@
 import { ActionTree } from 'vuex';
 import moment from 'moment-timezone';
 import { StateInterface } from '../index';
-import { ProductsInterface, ProductProcessRecord } from './state';
+import {
+  ProductInterface,
+  ProductsInterface,
+  ProductProcessRecord,
+} from './state';
 import { PageInfoInterface } from '../pageInfo/state';
 
-import data from '../../data/mockData';
+// import data from '../../data/mockData';
+
+import Products from '../../service/Products';
 
 const actions: ActionTree<ProductsInterface, StateInterface> = {
-  searchData({ commit }, payload: PageInfoInterface) {
+  async searchData({ commit }, payload: PageInfoInterface) {
     // your code
 
-    const filteredData = data.filter(
-      (item) => item.name.toLowerCase() === payload.name.toLowerCase(),
-    );
+    // const filteredData = data.filter(
+    //   (item) => item.name.toLowerCase() === payload.name.toLowerCase(),
+    // );
+
+    let filteredData = [] as ProductInterface[];
+
+    await Products(payload).then((res) => {
+      filteredData = res.content.products;
+    });
+
+    console.log(filteredData);
 
     const destructedArr = [] as ProductProcessRecord[];
+
     filteredData.forEach((product) => {
       const { id } = product;
       const productName = product.name;
@@ -22,6 +37,7 @@ const actions: ActionTree<ProductsInterface, StateInterface> = {
         const { operation, user } = machine;
         const machineName = machine.name;
         machine.processRecords.forEach((record) => {
+          const tz = record.timeZone ?? 'Asia/Taipei';
           const destrucedRecord: ProductProcessRecord = {
             id,
             name: productName,
@@ -29,21 +45,25 @@ const actions: ActionTree<ProductsInterface, StateInterface> = {
             operation,
             operator: user,
             machine: machineName,
+            // import: '',
+            // export: '',
+            // start: '',
+            // finished: '',
             import: moment
               .unix(record.importTime)
-              .tz(record.timeZone)
+              .tz(tz)
               .format('YYYY-MM-DD HH:mm:ss'),
             export: moment
               .unix(record.exportTime)
-              .tz(record.timeZone)
+              .tz(tz)
               .format('YYYY-MM-DD HH:mm:ss'),
             start: moment
               .unix(record.startProcessTime)
-              .tz(record.timeZone)
+              .tz(tz)
               .format('YYYY-MM-DD HH:mm:ss'),
             finished: moment
               .unix(record.finishProcessTime)
-              .tz(record.timeZone)
+              .tz(tz)
               .format('YYYY-MM-DD HH:mm:ss'),
           };
 
