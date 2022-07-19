b3a582a23220358386bd65d49859991955c06297
George Tsung <a0939988526@gmail.com>
2022-07-19 15:01:13 +0800

    fix(service): fix missing value when calling api

--- src/service/Products/records.ts
+++ src/service/Products/records.ts
@@ -14,7 +14,7 @@ const records = async (
 ): Promise<ProductsResponse> => {
   const data = await api
     .get(
-      `/ProcessingReports?start=${payload.from}&end=${payload.to}&from=${payload.shiftSelected.from}&to=${payload.shiftSelected.to}`,
+      `/ProcessingReports?start=${payload.from}&end=${payload.to}&from=${payload.shiftSelected.from}&to=${payload.shiftSelected.to}&productName=${payload.name}`,
     )
     .then((response) => response.data as ProductsResponse)
     .catch(() => {
