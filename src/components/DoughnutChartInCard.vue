a04033dec91f05a028dec9d20b458d6fdd7ded3a
George Tsung <a0939988526@gmail.com>
2022-08-01 14:53:04 +0800

    fix(Annotation): removed unused annotation

--- src/components/DoughnutChartInCard.vue
+++ src/components/DoughnutChartInCard.vue
@@ -14,7 +14,6 @@ import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
 export default defineComponent({
   name: 'DoughnutChartInCard',
-  // eslint-disable-next-line vue/no-unused-components
   components: { DoughnutChart },
   props: {
     actual: Number,
@@ -54,6 +53,16 @@ export default defineComponent({
     );
 
     const options = ref({
+      elements: {
+        center: {
+          text: 'Red is 2/3 the total numbers',
+          color: '#FF6384', // Default is #000000
+          fontStyle: 'Arial', // Default is Arial
+          sidePadding: 20, // Default is 20 (as a percentage)
+          minFontSize: 20, // Default is 20 (in px), set to false and text will not wrap.
+          lineHeight: 25, // Default is 25 (in px), used for when text wraps
+        },
+      },
       responsive: true,
       layout: {
         padding: {
